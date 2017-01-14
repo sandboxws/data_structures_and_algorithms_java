132fb5200f492a5662fc2dfcd5e2ed5c983da3d3
Ahmed El.Hussaini <ahussaini@souq.com>
2017-01-14 18:57:53 +0400

    removed Main.java and added it to .gitignore

--- src/main/java/com/sandboxws/hackerrank/Main.java
+++
@@ -1,9 +0,0 @@
-package com.sandboxws.hackerrank;
-
-import static java.lang.System.out;
-
-public class Main {
-    public static void main(String[] args) {
-	    out.println("Hello, world!");
-    }
-}
