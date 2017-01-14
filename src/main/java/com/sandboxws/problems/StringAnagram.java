c1fd08953660840928af2cc6fe26b1756dce538a
Ahmed El.Hussaini <ahussaini@souq.com>
2017-01-14 18:41:21 +0400

    added description to StringAnagram

--- src/main/java/com/sandboxws/problems/StringAnagram.java
+++ src/main/java/com/sandboxws/problems/StringAnagram.java
@@ -4,7 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by ahussaini on 1/14/17.
+ * Given two strings, check to see if they are anagrams.
+ * An anagram is when the two strings can be written using the exact same letters
+ * (so you can just rearrange the letters to get a different phrase or word).
+ * For example:
+ * "public relations" is an anagram of "crap built on lies."
+ * "clint eastwood" is an anagram of "old west action"
+ * Note: Ignore spaces and capitalization. So "d go" is an anagram of "God" and "dog" and "o d g".
  */
 public class StringAnagram {
     public boolean anagram(String s1, String s2) {
